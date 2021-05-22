869d90bb267ea5fa6a37ff93bc2c68337a50c5dd
Ignacio Riveros <ignacio.riveros.farfan@gmail.com>
2021-05-22 12:14:52 -0400

    add jQuery scrollspy & navbar styles on scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
 // wait for DOM ready
 $(document).ready(function () {
+  // BOOTSTRAP SCROLLSPY: change nav active state based on position
+  $('body').scrollspy({
+    target: '#myNav',
+    offset: 58,
+  });
   // BOOTSTRAP TOOLTIP
   $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+  });
+  // navbar styles on scroll
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $('.navbar').addClass('navbar-scrolled');
+    } else {
+      $('.navbar').removeClass('navbar-scrolled');
+    }
   });
 
   // Smooth scroll
